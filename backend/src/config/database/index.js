234d73bb9719a93ea0529f40102688c9a28a0307
Cristofer Martins <cristofersantosmartins@outlook.com>
2025-03-22 18:42:42 -0300

    fix MYSQL_PORT env variable being ignored, set MYSQL_HOST and MYSQL_PORT to default values in .env.example

--- backend/src/config/database/index.js
+++ backend/src/config/database/index.js
@@ -6,7 +6,8 @@ const sequelize = new Sequelize(
     process.env.MYSQL_USER,
     process.env.MYSQL_PASSWORD,
     {
-        host: process.env.MYSQL_HOST || "host.docker.internal",
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
         dialect: "mysql",
         logging: false,
     }
